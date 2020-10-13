db09627ba5e98390c755710fe1447c17aaa6d76e
Samk13 <samarbid13@gmail.com>
2020-10-13 22:00:14 +0200

    create model ciontroller and migration

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('body')
-<section class="bg-gray-900 h-screen md:px-5 pt-10 ">
+<section class="bg-gray-900 h-screen md:px-5 pt-10 text-indigo-900 ">
     <div class="p-5 bg-yellow-500 rounded-lg space-y-4">
         <h1 class="uppercase text-3xl font-serif">Lorem, ipsum dolor sit font-display font-black</h1>
 
@@ -10,7 +10,9 @@
             amet consectetur adipisicing elit. Eligendi autem provident, distinctio magnam error blanditiis est enim minima voluptate delectus doloremque beatae quae eveniet impedit! Cumque exercitationem consequatur ducimus vitae.
 
         </div>
-        <div  class="bg-blue-500 rounded-lg p-2 font-black">
+        <div class="bg-indigo-900 h-1"></div>
+        <!-- <div class="bg-teal-900 text-pink-700"></div> -->
+        <div  class="shadow-lg rounded-lg p-2 font-black">
             <table class="table-auto">
                 <thead>
                     <tr>
